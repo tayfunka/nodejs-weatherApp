b244b13a1e84926cac6338ae81b60be2c07abe54
tayfun karakavuz <tayfun.karakavuz@gmail.com>
2023-10-31 01:23:23 +0300

    v1

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const MAPBOX_KEY = process.env.MAPBOX_KEY;
 
 const geocode = (adress, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(adress) + '.json?access_token=' + MAPBOX_KEY + '&limit=1'
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(adress) + '.json?access_token=' + MAPBOX_KEY + '&limit=1'
     request({ url, json: true }, (error, { body }) => { // (error, response) = callback
         if (error) {
             callback('Unable to connet to location services!', undefined)
